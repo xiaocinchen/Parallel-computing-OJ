594a598e1414ebc7e6b404921b1301837bbe1724
xiaocinchen <707103676@qq.com>
2023-06-20 18:59:20 +0800

    add SpringSecurity module

--- src/main/java/com/offer/oj/OjApplication.java
+++ src/main/java/com/offer/oj/OjApplication.java
@@ -1,11 +1,15 @@
 package com.offer.oj;
 
 import com.alicp.jetcache.anno.config.EnableMethodCache;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
 @EnableMethodCache(basePackages = "com.offer.oj")
+@MapperScan(basePackages = "com.offer.oj.dao.mapper")
+@EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
 public class OjApplication {
 
     public static void main(String[] args) {
