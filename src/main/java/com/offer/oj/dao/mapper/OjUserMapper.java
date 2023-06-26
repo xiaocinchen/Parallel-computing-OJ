c63b91318a8320424fd56b6f528e48869b68993d
xiaocinchen <707103676@qq.com>
2023-06-26 16:29:12 +0800

    add register_cache module and logging to file

--- src/main/java/com/offer/oj/dao/mapper/OjUserMapper.java
+++ src/main/java/com/offer/oj/dao/mapper/OjUserMapper.java
@@ -1,6 +1,10 @@
 package com.offer.oj.dao.mapper;
 
+import com.alicp.jetcache.anno.CacheType;
+import com.alicp.jetcache.anno.Cached;
 import com.offer.oj.domain.OjUser;
+
+import java.util.concurrent.TimeUnit;
 
 public interface OjUserMapper {
     int deleteByPrimaryKey(Integer id);
@@ -15,5 +19,6 @@ public interface OjUserMapper {
 
     int updateByPrimaryKey(OjUser row);
 
+    @Cached(name = "userCache", key = "#username", expire = 300, timeUnit = TimeUnit.SECONDS, cacheType = CacheType.REMOTE)
     OjUser selectByUsername(String username);
 }
