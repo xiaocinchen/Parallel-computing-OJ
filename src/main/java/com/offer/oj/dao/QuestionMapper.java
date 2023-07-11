d6d397d55fd2b5a6759046432d1fe9d5b1209e34
yueliang-SDK <2969707796@qq.com>
2023-07-11 14:33:13 +0800

    “modify Kaptcha and add forget password"
    # Conflicts:
    #	src/main/java/com/offer/oj/controller/KaptchaController.java
    #	src/main/java/com/offer/oj/service/KaptchaService.java
    #	src/main/java/com/offer/oj/service/impl/KaptchaServiceImpl.java
    #	src/test/java/com/offer/oj/OjApplicationTests.java

--- src/main/java/com/offer/oj/dao/QuestionMapper.java
+++ src/main/java/com/offer/oj/dao/QuestionMapper.java
@@ -1,6 +1,7 @@
 package com.offer.oj.dao;
 
 import com.offer.oj.domain.OjQuestion;
+import com.offer.oj.domain.dto.QuestionDTO;
 
 public interface QuestionMapper {
     void insertSelective(OjQuestion row);
